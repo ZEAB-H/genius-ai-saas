61312f4a94082d74c88e2b237d09247359224032
ZEAB-H <abelhailemichael30@gmail.com>
2023-08-03 14:57:39 +0300

    finished the setup and started the folder structure for the routes

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,10 @@
 import { Button } from "@/components/ui/button"
 import { DESTRUCTION } from "dns"
 
-export default function Home() {
+export default function Dashboard() {
   return (
     <p> 
-      <p className="text-6xl text-green-400">Hello ethiopia!</p>
+      <p className="text-6xl text-green-400">Dashboard Page!</p>
       <p><Button variant="destructive">Click Me!</Button></p>
       </p>
   )
